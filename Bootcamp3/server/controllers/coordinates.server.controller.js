cc2bc8c2c5f77b952e95b4de909cd1fc5f2c815b
ywilliamson20 <ywilliamson20@ufl.edu>
2019-10-02 20:29:20 -0400

    Final

--- Bootcamp3/server/controllers/coordinates.server.controller.js
+++ Bootcamp3/server/controllers/coordinates.server.controller.js
@@ -1,4 +1,4 @@
-var config = require('../config/config'), 
+var config = require('../config/config'),
     request = require('request');
 
 
@@ -10,32 +10,44 @@ module.exports = function(req, res, next) {
       var addressTemp2 = addressTemp.toLowerCase();
       var addressTemp3 = addressTemp2.replace(/\s/g, "%20");
       var addressTemp4 = addressTemp3.replace(/,/g , "%2C");
-      
-    //Setup your options q and key are provided. Feel free to add others to make the JSON response less verbose and easier to read 
-    var options = { 
+
+    //Setup your options q and key are provided. Feel free to add others to make the JSON response less verbose and easier to read
+    var options = {
       q: addressTemp4,
-      key: config.openCage.key,  
+      key: config.openCage.key,
     }
 
     //Setup your request using URL and options - see ? for format
     request({
-      url: 'https://api.opencagedata.com/geocode/v1/json', 
+      url: 'https://api.opencagedata.com/geocode/v1/json',
       qs: options
       }, function(error, response, body) {
         //For ideas about response and error processing see https://opencagedata.com/tutorials/geocode-in-nodejs
-        
+
         //JSON.parse to get contents. Remember to look at the response's JSON format in open cage data
-        
-        /*Save the coordinates in req.results -> 
-          this information will be accessed by listings.server.model.js 
+
+        /*Save the coordinates in req.results ->
+          this information will be accessed by listings.server.model.js
           to add the coordinates to the listing request to be saved to the database.
 
           Assumption: if we get a result we will take the coordinates from the first result returned
         */
         //  req.results = stores you coordinates
+      
+
+        if (error) {
+              throw error;
+            }
+
+
+
+        var data=JSON.parse(body);
+        req.results = data.results[0].geometry;
         next();
-    });
+
+});
+
   } else {
-    next();
+   next();
   }
-};  
+};
