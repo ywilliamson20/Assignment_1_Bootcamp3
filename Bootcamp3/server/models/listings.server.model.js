b23846a1c0436cb566caf77ef2075fe139376649
ywilliamson20 <ywilliamson20@ufl.edu>
2019-10-02 20:27:18 -0400

    Final

--- Bootcamp3/server/models/listings.server.model.js
+++ Bootcamp3/server/models/listings.server.model.js
@@ -1,18 +1,42 @@
 //You can replace this entire file with your Bootcamp Assignment #2 - ListingSchema.js File
 
 /* Import mongoose and define any variables needed to create the schema */
-var mongoose = require('mongoose'), 
+var mongoose = require('mongoose'),
     Schema = mongoose.Schema;
 
 /* Create your schema */
 var listingSchema = new Schema({
   /* your code here from Bootcamp Assignment #2 - ListingSchema.js File*/
-    
+  code: {
+          type: String,
+          required: true,
+      },
+  name:{
+          type: String,
+          required: true,
+      },
+ coordinates: {
+        latitude: Number,
+        longitude: Number
+                },
+ address:String,
+created_at: Date,
+updated_at: Date
+
 });
 
 /* create a 'pre' function that adds the updated_at and created_at if not already there property */
 listingSchema.pre('save', function(next) {
   /* your code here from Bootcamp Assignment #2 - ListingSchema.js File */
+  // get the current date
+  var currentDate = new Date();
+
+  // change the updated_at field to current date
+  this.updated_at = currentDate;
+
+  // if created_at doesn't exist, add to that field
+  if (!this.created_at)
+    this.created_at = currentDate;
 
   next();
 });
